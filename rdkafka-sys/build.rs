178855bf76cf03b42723ce236d990816fcf305cb
gyfis <gyfis@seznam.cz>
2022-06-29 23:29:26 +0200

    Test if rdkafka-sys builds with curl explicitly disabled

--- rdkafka-sys/build.rs
+++ rdkafka-sys/build.rs
@@ -218,18 +218,19 @@ fn build_librdkafka() {
         config.define("WITH_ZLIB", "0");
     }
 
-    if env::var("CARGO_FEATURE_CURL").is_ok() {
-        config.define("WITH_CURL", "1");
-        config.register_dep("curl");
-        if let Ok(curl_root) = env::var("DEP_CURL_ROOT") {
-            config.define("CURL_STATICLIB", "1");
-            // env::set_var("CMAKE_LIBRARY_PATH", format!("{}/lib", curl_root));
-            config.cflag(format!("-I{}/include", curl_root));
-            config.cxxflag(format!("-I{}/include", curl_root));
-        }
-    } else {
-        config.define("WITH_CURL", "0");
-    }
+    config.define("WITH_CURL", "0");
+    // if env::var("CARGO_FEATURE_CURL").is_ok() {
+    //     config.define("WITH_CURL", "1");
+    //     config.register_dep("curl");
+    //     if let Ok(curl_root) = env::var("DEP_CURL_ROOT") {
+    //         config.define("CURL_STATICLIB", "1");
+    //         // env::set_var("CMAKE_LIBRARY_PATH", format!("{}/lib", curl_root));
+    //         config.cflag(format!("-I{}/include", curl_root));
+    //         config.cxxflag(format!("-I{}/include", curl_root));
+    //     }
+    // } else {
+    //     config.define("WITH_CURL", "0");
+    // }
 
     if env::var("CARGO_FEATURE_SSL").is_ok() {
         config.define("WITH_SSL", "1");
