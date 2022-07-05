197a1c80415337ac07bf186cb90505d3ff6b8de0
gyfis <gyfis@seznam.cz>
2022-07-05 08:23:11 +0200

    Seems that we also need the CMAKE_LIBRARY_PATH

--- rdkafka-sys/build.rs
+++ rdkafka-sys/build.rs
@@ -128,7 +128,7 @@ fn build_librdkafka() {
     if env::var("CARGO_FEATURE_CURL").is_ok() {
         // There is no --enable-curl option, but it is enabled by default.
         if let Ok(curl_root) = env::var("DEP_CURL_ROOT") {
-            cflags.push("-DCURLSTATIC_LIB");
+            cflags.push("-DCURLSTATIC_LIB".to_string());
             ldflags.push(format!("-L{}/build", curl_root));
         }
     } else {
@@ -197,6 +197,7 @@ fn build_librdkafka() {
 #[cfg(feature = "cmake-build")]
 fn build_librdkafka() {
     let mut config = cmake::Config::new("librdkafka");
+    let mut cmake_library_paths = vec![];
 
     config
         .define("RDKAFKA_BUILD_STATIC", "1")
@@ -212,7 +213,7 @@ fn build_librdkafka() {
         config.define("WITH_ZLIB", "1");
         config.register_dep("z");
         if let Ok(z_root) = env::var("DEP_Z_ROOT") {
-            env::set_var("CMAKE_LIBRARY_PATH", format!("{}/build", z_root));
+            cmake_library_paths.push(format!("{}/build", z_root));
         }
     } else {
         config.define("WITH_ZLIB", "0");
@@ -223,6 +224,7 @@ fn build_librdkafka() {
         config.register_dep("curl");
         if env::var("DEP_CURL_ROOT").is_ok() {
             config.define("CURL_STATICLIB", "1");
+            cmake_library_paths.push(format!("{}/lib", curl_root));
             config.cflag("-DCURL_STATICLIB");
             config.cxxflag("-DCURL_STATICLIB");
         }
@@ -266,6 +268,10 @@ fn build_librdkafka() {
 
     if let Ok(system_name) = env::var("CMAKE_SYSTEM_NAME") {
         config.define("CMAKE_SYSTEM_NAME", system_name);
+    }
+
+    if !cmake_library_paths.is_empty() {
+        env::set_var("CMAKE_LIBRARY_PATH", cmake_library_paths.join(";"));
     }
 
     println!("Configuring and compiling librdkafka");
